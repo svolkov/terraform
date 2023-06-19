0412e89a8d0febf57db34e77483eb02dd44193c9
Serhii Volkov <svolkovua@google.com>
2023-06-19 17:17:00 +0000

    Create folder for TF

---
+++ tf/variables.tf
@@ -0,0 +1,10 @@
+variable "GOOGLE_REGION" {
+  type = string
+  default = "us-cenral1-c"
+  description = "GCP region name"
+}
+
+variable "GOOGLE_PROJECT" {
+  type = string
+  description = "GCP project name"
+}
